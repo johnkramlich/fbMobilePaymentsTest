d0545906173ddae5f69d61ca0111d3a7bba4f9e7
John Kramlich <me@johnkramlich.com>
2012-10-21 19:42:23 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+	
+	$('#login-btn').click(function(){
+		FB.login(function(response) {
+	   if (response.authResponse) {
+	     console.log('Welcome!  Fetching your information.... ');
+	 FB.api('/me', function(response) {
+	   console.log('Good to see you, ' + response.name + '.');
+	     });
+	   } else {
+	     console.log('User cancelled login or did not fully authorize.');
+	   }
+	 });
+	});
+	
+	
+	$('#buy-it-btn').click(function(){
+		
+		if( ! FB.UA.nativeApp()){
+			FB.ui(
+				{
+					method: 'pay',
+					action: 'buy_item',
+					// order_info can be anything you want; it will be passed to your 
+					// Payments callback's payments_get_items function, which should 
+					// be able to process and respond to this data.
+					order_info: {'item_id': '1a'}
+				},
+				function(result){
+					if(typeof result.order_id !== 'undefined'){
+						// Succcess
+						alert('thank you for your order');
+					} else {
+						//Failure or cancel
+						alert('order canceled');
+					}
+				}
+			);
+		} else {
+			alert('Sorry Buddy, no facebook purchases in a native iOS app!');
+		}
+		
+		
+		
+	});
+	
+	
+	
+	
+	
+	
+	
+	
+	
+});
